ff14d910e770c37bf70257e3c0291694a1665817
Unknown <brian_madina@utexas.edu>
2016-11-01 17:30:00 -0500

    Imported JavaFX files to main and added buttons

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -12,11 +12,40 @@
  */
 
 package assignment5;
-
-public class Main {
-
-	public static void main(String[] args) {
-		// launch(args);
-	}
-
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    Button btnquit, btnshow, btnstep, btnseed, btnmake, btnstats;
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Critters2");
+        btnquit = new Button("quit");
+        btnshow = new Button("show");
+        btnstep = new Button("step");
+        btnseed = new Button("seed");
+        btnmake = new Button("make");
+        btnstats = new Button("stats")
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
 }

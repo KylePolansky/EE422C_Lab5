760e681e31e5601e5290773705ff81b3c8c57f10
Kyle Polansky <KylePolansky@gmail.com>
2016-11-01 13:32:40 -0500

    Add some headers

--- src/assignment5/Header.java
+++ src/assignment5/Header.java
@@ -1,12 +1,12 @@
 /* CRITTERS GUI <MyClass.java>
  * EE422C Project 4b submission by
  * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
- * <Student1 5-digit Unique No.>
- * <Student2 Name>
- * <Student2 EID>
- * <Student2 5-digit Unique No.>
+ * <Brian Madina>
+ * <bjm3348>
+ * <16460>
+ * <Kyle Polansky>
+ * <KPP446>
+ * <16480>
  * Slip days used: <0>
  * Fall 2016
  */
--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -1,3 +1,16 @@
+/* CRITTERS GUI <Main.java>
+ * EE422C Project 4b submission by
+ * Replace <...> with your actual data.
+ * <Brian Madina>
+ * <bjm3348>
+ * <16460>
+ * <Kyle Polansky>
+ * <KPP446>
+ * <16480>
+ * Slip days used: <0>
+ * Fall 2016
+ */
+
 package assignment5;
 
 public class Main {

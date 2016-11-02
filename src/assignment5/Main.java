58ae886434516de74d107a4004aa193ae04d2a06
Unknown <brian_madina@utexas.edu>
2016-11-01 20:26:08 -0500

    Implemented reflection, extra textfields and buttons, added lables

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -28,8 +28,13 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    private Button btnquit, btnshow, btnstep, btnseed, btnmake, btnstats;
-    private TextField stepsInput;
+    private static String myPackage;	// package of Critter file.  Critter cannot be in default pkg.
+    static {
+        myPackage = Critter.class.getPackage().toString().split(" ")[1];
+    }
+    private Button btnquit, btnshow, btnstep, btnseed, btnmake, btnstats, btnanimation, btnFpsUp, btnFpsDown;
+    private TextField stepsInput, makeInput, makeInput2, statsInput, seedInput, fpsInput;
+    private Label stepsLabel, makeLabel, makeLabel2, statsLabel, seedLabel, fpsLabel;
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Critters2");
@@ -40,19 +45,53 @@ public class Main extends Application {
         btnmake = new Button("make");
         btnstats = new Button("stats");
         stepsInput = new TextField();
+        makeInput = new TextField();
+        statsInput = new TextField();
+        seedInput = new TextField();
+        makeInput2 = new TextField();
+        btnanimation = new Button("Animate");
+        fpsInput = new TextField();
+        btnFpsDown = new Button("-");
+        btnFpsUp = new Button("+");
+        stepsLabel = new Label("Number of Time Steps");
+        makeLabel = new Label("Critter Name");
+        makeLabel2 = new Label("Number of Critters");
+        statsLabel = new Label("Critter Name");
+        seedLabel = new Label("Seed Number");
+        fpsLabel = new Label("Time Steps per Frame");
                
       
         GridPane root = new GridPane();
         root.setHgap(20);
         root.setVgap(20);
         root.add(btnquit,0,0);
+        
         root.add(btnshow,0,1);
-        root.add(btnstep,0,2);
-        root.add(stepsInput, 1,2);
-        root.add(btnseed,0,3);
-        root.add(btnmake,0,4);
-        root.add(btnstats,0,5);
-        Scene scene = new Scene(root, 500, 500);
+        
+        root.add(stepsLabel,1,2);
+        root.add(btnstep,0,3);
+        root.add(stepsInput, 1,3);
+        
+        root.add(seedLabel,1,4);
+        root.add(btnseed,0,5);
+        root.add(seedInput, 1,5);
+        
+        root.add(makeLabel,1,6);
+        root.add(makeLabel2,2,6);
+        root.add(btnmake,0,7);
+        root.add(makeInput, 1, 7);
+        root.add(makeInput2, 2, 7);
+        
+        root.add(statsLabel, 1, 8);
+        root.add(btnstats,0,9);
+        root.add(statsInput, 1, 9);
+        
+        root.add(fpsLabel, 1, 10);
+        root.add(btnanimation,0,11);
+        root.add(fpsInput, 1, 11);
+        root.add(btnFpsUp, 3, 11);
+        root.add(btnFpsDown, 2, 11);
+        Scene scene = new Scene(root, 600, 600);
         buttonToCode();
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -79,16 +118,52 @@ public class Main extends Application {
         {
         	Critter.displayWorld();
         }
-        /*else if(e.getSource()==btnstep)
-        {
-        	int numSteps = Integer.parseInt(stepsInput.getText());;
-            if ( == 2) {
-                numSteps = Integer.parseInt(inputSplit[1]);
-            }
+        else if(e.getSource()==btnstep)
+        {	
+        	int numSteps;
+        	if(stepsInput.getText().equals(""))
+        		numSteps = 1;
+        	else 
+        		numSteps = Integer.parseInt(stepsInput.getText());
             for (int i = 0; i < numSteps; i++) {
                 Critter.worldTimeStep();
             }
-        }*/
+        }
+        else if(e.getSource()==btnseed)
+        {
+        	int seed;
+        	if(seedInput.getText().equals(""))
+        		seed = 0;
+        	else
+        		seed = Integer.parseInt(seedInput.getText());
+            Critter.setSeed(seed);
+            
+        }
+        else if(e.getSource()==btnmake)
+        {	
+        	int numCritters;
+        	if(makeInput2.getText().equals(""))
+        		numCritters = 1;
+        	else
+        		numCritters = Integer.parseInt(makeInput2.getText());
+        	try{
+            for (int i = 0; i < numCritters; i++) {
+                Critter.makeCritter(myPackage + "." + makeInput.getText()); //prepend the package name
+            }
+        	}catch(Exception e1)
+        	{
+        		System.out.print("You fucked up");
+        	}
+            
+        }
+        else if(e.getSource() == btnstats)
+        {
+        	
+        }
+        else if(e.getSource() == btnstats)
+        {
+        	
+        }
        
     }
 
